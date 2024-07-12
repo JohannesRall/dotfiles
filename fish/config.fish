020ef920101c36ed4b52270f4ade96ea25d1c1da
Johannes Rall <johannes.rall@proton.me>
2024-07-12 16:41:02 +0200

    Change stuff

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+set -g fish_greeting
+
+if status is-interactive
+    starship init fish | source
+end
+
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+abbr ytd 'yt-dlp -xo "$HOME/Music/%(title)s.%(ext)s" 
+'
+
+
+alias z="zoxide"
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
+
+zoxide init fish | source
