badbc16b3be4d2a45d2aafea907a86877ada4bfd
Johannes Rall <johannes.rall@proton.me>
2024-07-14 10:47:42 +0200

    fish update

--- fish/config.fish
+++ fish/config.fish
@@ -10,11 +10,9 @@ abbr ... 'cd ../..'
 abbr .3 'cd ../../..'
 abbr .4 'cd ../../../..'
 abbr .5 'cd ../../../../..'
-abbr ytd 'yt-dlp -xo "$HOME/Music/%(title)s.%(ext)s" 
-'
-
-
-alias z="zoxide"
+abbr ytd 'yt-dlp -xo "$HOME/Music/%(title)s.%(ext)s"'
+abbr z zoxide
+abbr g git
 
 # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
